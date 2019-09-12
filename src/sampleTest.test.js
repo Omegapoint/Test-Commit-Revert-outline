20475094ce75b2fc99bd1c241984e0cb72c2410d
Martin Carlsson <martin.carlsson@svt.se>
2019-09-12 21:55:02 +0200

    Sample tests.

--- src/sampleTest.test.js
+++ src/sampleTest.test.js
@@ -1,4 +1,14 @@
+var {Shop, Item} = require('./gilded_rose.js');
+
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
   });
-  
+
+
+test('Empty shop should return no items', () => {
+   expect(new Shop([]).updateQuality()).toHaveLength(0);
+});
+
+test('Shop with one item should return some item', () => {
+    expect(new Shop([new Item("name", 1, 2)]).updateQuality()).toHaveLength(1);
+});
